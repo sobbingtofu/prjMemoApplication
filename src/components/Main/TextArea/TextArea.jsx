bbf39088b805854c2ed4283e518cba39ea0f6180
sobbingtofu <kukubasak@naver.com>
2024-05-27 15:09:13 +0900

    update: textarea component error resolved

--- src/components/Main/TextArea/TextArea.jsx
+++ src/components/Main/TextArea/TextArea.jsx
@@ -1,11 +1,8 @@
 import {MemoInputSection} from "../MainSection.styledcomp";
-import {useEffect, useState, useRef} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {UPDATE_MEMO} from "../../../redux/modules/memo";
 
 const TextArea = () => {
-  const textAreaRef = useRef(null);
-
   const memoList = useSelector((state) => {
     return state.memo.memos;
   });
@@ -14,40 +11,19 @@ const TextArea = () => {
     return state.memo.selectedMemoID;
   });
 
-  const selectedMemoIndex = memoList.findIndex((memo) => {
+  const selectedMemo = memoList.find((memo) => {
     return memo.id == selectedMemoID;
   });
 
-  const textareaValue = memoList[selectedMemoIndex] ? memoList[selectedMemoIndex].content : "";
-
-  const [inputText, setInputText] = useState("");
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (inputText !== "") {
-      dispatch(UPDATE_MEMO({inputText}));
-      // dispatch(SET_TEXT_AREA(inputText));
-    }
-  }, [inputText, dispatch]);
-
-  if (textAreaRef.current !== null) {
-    textAreaRef.current.focus();
-  }
-
   const handleMemoChange = (event) => {
-    if (event.target.value === "") {
-      setInputText(event.target.value);
-      dispatch(UPDATE_MEMO({inputText: ""}));
-      // dispatch(SET_TEXT_AREA(""));
-    } else {
-      setInputText(event.target.value);
-    }
+    dispatch(UPDATE_MEMO({inputText: event.target.value}));
   };
 
   return (
     <>
-      {/* <MemoInputSection type="text" autoFocus onChange={handleMemoChange} value={inputText ? inputText : textareaValue} ref={textAreaRef} /> */}
-      <MemoInputSection type="text" autoFocus onChange={handleMemoChange} value={textareaValue ? textareaValue : inputText} ref={textAreaRef} />
+      <MemoInputSection type="text" autoFocus onChange={handleMemoChange} value={selectedMemo?.content || ""} />
     </>
   );
 };
